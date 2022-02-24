fcac82d0f8e5fbc7fd54d9c64870ad88552170bd
Nguyen Huynh <huynhn.tma@orangetheoryfitness.com>
2022-02-24 15:19:20 +0700

    Init VVAExport

---
+++ OT.VVAExport/Models/VVASlide.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace OT.VVAExport.Models
+{
+    public class VVASlide
+    {
+        public string BlockId { get; set; }
+        public string Header { get; set; }
+        public string Duration { get; set; }
+        public IEnumerable<VVAExercise> Exercises { get; set; }
+    }
+}
--- OT.VVAExport/Program.cs
+++ OT.VVAExport/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Packaging;
+using Microsoft.CSharp;
+using Serialize.OpenXml.CodeGen;
+using System;
+using System.IO;
 
 namespace OT.VVAExport
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var slides = JsonConvert.DeserializeObject<IEnumerable<VVASlide>>(System.IO.File.ReadAllText("./data.json"));
+
+            var sourceFile = new FileInfo(@"./Sample1.xlsx");
+            var targetFile = new FileInfo(@"./Sample1.cs");
+
+            using (var source = sourceFile.Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var ppt = PresentationDocument.Open(source, false))
+                {
+                    if (ppt != null)
+                    {
+                        // Generate VB.NET source code
+                        var cSharp = new CSharpCodeProvider();
+
+                        // Save the source code to the target file
+                        using (var target = targetFile.Open(FileMode.Create, FileAccess.ReadWrite))
+                        {
+                            using (var tw = new StreamWriter(target))
+                            {
+                                // Providing the CodeDomProvider object as a parameter will
+                                // cause the method to return the source code as a string
+                                tw.Write(ppt.GenerateSourceCode(cSharp).Trim());
+                            }
+                            target.Close();
+                        }
+                    }
+                }
+                source.Close();
+            }
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey(true);
         }
     }
 }
